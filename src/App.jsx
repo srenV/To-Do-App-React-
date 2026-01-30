ea4cc7262d6548293f2056c3ef62cd557e908cd5
srenV <225722508+srenV@users.noreply.github.com>
2026-01-30 15:22:46 +0100

    add bg batch

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import { BadgeCheck } from "lucide-react";
 import { TodoProvider } from "./context/TodoContext.jsx";
 import { Main } from "./layout/Main.jsx";
 
 function App() {
   return (
     <TodoProvider>
+      <BadgeCheck className="absolute my-auto mx-auto inset-0 scale-1000 text-gray-200/10"/>
       <div className=" flex flex-col h-svh gap-5 bg-[#F2F0EF] dark:bg-gray-950">
+        
         <Main />
       </div>
     </TodoProvider>
