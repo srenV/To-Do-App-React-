5eb591aabd1ad2abb11a184cdd78aa637b7551a8
srenV <225722508+srenV@users.noreply.github.com>
2026-01-30 17:00:48 +0100

    animation change for entry

--- src/App.jsx
+++ src/App.jsx
@@ -21,9 +21,9 @@ function App() {
               initial={{ opacity: 0}}
               animate={{ opacity: 1}}
               exit={{ opacity: 0 }}
-              className="fixed inset-0 bg-black z-50 w-svw h-svh"
+              className="fixed inset-0 bg-[#F2F0EF] dark:bg-gray-950 z-50 w-svw h-svh"
             >
-              <BadgeCheck className="absolute my-auto mx-auto inset-0 scale-1000 text-gray-200/10  animate-bounce" />
+              <BadgeCheck className="absolute my-auto mx-auto inset-0 scale-1000 text-gray-200/10  animate-ping" />
             </motion.div>
           )}
         </AnimatePresence>
